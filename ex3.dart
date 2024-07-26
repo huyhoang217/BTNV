6f3e620154a66e6d4cbc4f77e0069e1c101944f6
name <huyhoang21072@gmail.com>
2024-07-26 19:13:20 +0700

    fix bug

--- ex3.dart
+++ ex3.dart
@@ -9,7 +9,7 @@ List<int>? sumTarget(List<int> arr, int target){
     return [];
   }
   for(int i1 = 0; i1< arr.length; i1++){
-    for(int i2 = i1; i2< arr.length; i2++){
+    for(int i2 = i1 + 1; i2< arr.length; i2++){
       if(arr[i1] + arr[i2] == target){
         return [i1,i2];
       }
